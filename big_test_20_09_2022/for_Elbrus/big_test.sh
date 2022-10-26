e7490b98f1b9e2babb45950510ea92707bafd83f
IvanLix0603 <66360031+IvanLix0603@users.noreply.github.com>
2022-10-26 20:55:09 +0300

    Delete big_test_20_09_2022 directory

--- big_test_20_09_2022/for_Elbrus/big_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "1_test:"
-cd 1_test;bash scr2.sh;cd ..
-echo "2_test:"
-cd 2_test;bash scr2.sh;cd ..
-echo "3_test:"
-cd 3_test;bash scr2.sh;cd ..
-echo "4_test:"
-cd 4_test;bash scr2.sh;cd ..
